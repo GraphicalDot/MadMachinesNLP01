fab8b31c20e9fe014b0d1e1e4e0ae9dbb05fce93
kaali-python <root@kaali.(none)>
2014-10-06 16:56:30 +0530

    Update model files

--- Text_Processing/CustomSentenceTokenizer.py
+++ Text_Processing/CustomSentenceTokenizer.py
@@ -9,7 +9,7 @@ import string
 
 # Tokenize text into words, punctuation, and whitespace tokens
 
-class ModifiedWPTokenizer( nltk.tokenize.RegexpTokenizer):
+class ModifiedWPTokenizer(nltk.tokenize.RegexpTokenizer):
 	def __init__(self):
 		nltk.tokenize.RegexpTokenizer.__init__(self, r'\w+|[^\w\s]|\s+')
 
